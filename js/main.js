839ee4332b0d1371f047bcecad6f52893c8e07d8
heraudk <72447916+heraudk@users.noreply.github.com>
2020-11-26 09:05:06 +0100

    jquery exo

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+    $(".bouton1").click(function(){
+        $("#div1").slideToggle(10000); //slideUp() et slideDown()
+    });
+
+
+    $(".bouton1").click(function(){
+        $("#div2").slideToggle(10000); //slideUp() et slideDown()
+    });
+    
+
+
+
+
+    $(".bouton2").click(function(){
+        $("#div1").toggleClass('vert', true);
+    });
+
+    $(".bouton2").click(function(){
+        $("#div2").toggleClass('vert', true);
+    });
+
+ 
+
+
+  
